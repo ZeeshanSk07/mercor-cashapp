e0dcb3ff0f3d13e9dcef92cefd3a0783d5cbc3b7
Zeeshan Sheikh <127923500+ZeeshanSk07@users.noreply.github.com>
2023-08-03 17:00:24 +0530

    1st page done

---
+++ src/Components/Main.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Hero from './Pages/Hero';
+import Section2 from './Pages/Section2';
+import Section3 from './Pages/Section3';
+import Section4 from './Pages/Section4';
+import Section5 from './Pages/Section5';
+
+const Main = () => {
+    return (
+       <>
+        <Hero/>
+        <Section2/>
+        <Section3/>
+        <Section4/>
+        <Section5/>
+       </>
+    );
+};
+
+export default Main;
